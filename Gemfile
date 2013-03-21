001f7eac41d6a2bfc160129101ba235e2f940d29
maartenvg <git@maartenvangrootel.nl>
2013-03-21 13:14:09 -0400

    Add Gemfile, config.ru and some bug fixes

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+gem 'sinatra'
+gem 'nokogiri'
+gem 'datamapper'
+gem 'dm-postgres-adapter'
--- home.rb
+++ home.rb
@@ -3,47 +3,34 @@ require 'sinatra'
 require 'date'
 require 'open-uri'
 require 'nokogiri'
+require 'data_mapper'
+
+class Event
+  include DataMapper::Resource
+
+  property :id,           Serial
+  property :date,         Date,     :index => true
+  property :description,  String ,  :length => 250
+end
+
+DataMapper::Logger.new(STDOUT, :debug)
+DataMapper.setup(:default, "postgres://conception:Test1234@localhost/conception")
 
 get '/' do
   erb :index
 end
 
 post '/' do
-  @lang = params[:lang]
-  @date = Date.parse(params[:date]) << 9
-  
-  #options = []
-  #options << try_exact_date()
-  #options << try_day_month()
-  #options << try_month_year()
-  #options << try_year_only()
-  
-  event = try_exact_date()
-  "Most likely historical event that aroused your parents around #{@date}: #{event}"
-end
+  begin
+    date = Date.parse(params[:date]) << 9
 
-get '/scrape' do
-  
+    event = Event.all(:date.lte => date, :date.gt => date - 14, :order => [ :date.desc ])[0]
+    if event
+      "Most likely historical event that aroused your parents<br /> #{event[:date]}: #{event[:description]}"
+    else
+      "Nothing significant happend in the two weeks before your conception around #{date}."
+    end
+  rescue ArgumentError
+    halt 500, 'Illegal date'
+  end
 end
-
-def try_exact_date
-  from = (@date - 3).to_time.strftime("%Y%m%d")
-  to = (@date + 3).to_time.strftime("%Y%m%d")
-  url = "http://www.vizgr.org/historical-events/search.php?begin_date=#{from}&end_date=#{to}&lang=#{@lang}&granularity=all"
-  puts url
-  doc = Nokogiri::HTML(open(url))
-  doc.xpath('//description')[0] 
-end
-
-def try_day_month
-  
-end
-
-def try_month_year
-  
-end
-
-def try_year_only
-  
-end
-
